b9307bbc9675dbd36d47d8b8fafe8ea3e1a9503f
AKLLandscaping <info@akllandscaping.com>
2025-03-31 22:29:48 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 def calculate_material_delivery(loads, distances, base_prices):
     HST_RATE = 0.15
     EXTRA_KM_RATE = 4.20
@@ -41,33 +40,40 @@ def calculate_material_delivery(loads, distances, base_prices):
         "total": round(grand_total, 2)
     }
 
-def calculate_sod_quote(area, laborers, labor_rate, labor_hours, machines, travel_km, pallet_price=25, pallet_coverage=300):
-    HST_RATE = 0.15
+def calculate_sod_quote(area, labor_inputs, equipment_inputs, travel_inputs, gravel_inputs, stay_inputs, pallet_price=25, pallet_coverage=300):
+    HST = 0.15
     SOD_COST_PER_SQFT = 1.20
-    TRAILER_BASE_COST = 250
+    TRAILER_BASE = 250
     TRAILER_KM_RATE = 4.20
     TRAILER_INCLUDED_KM = 30
     VEHICLE_KM_RATE = 0.80
 
-    pallets = -(-area // pallet_coverage)  # ceil division
+    pallets = -(-area // pallet_coverage)
     sod_cost = area * SOD_COST_PER_SQFT
     pallet_cost = pallets * pallet_price
 
-    labor_cost = laborers * labor_hours * labor_rate
+    labor_cost = sum(l["rate"] * l["hours"] for l in labor_inputs)
 
     equipment_cost = 0
-    for m in machines:
-        qty = m["qty"]
-        rate = m["hourly_rate"]
-        hours = m["hours"]
-        trailer_km = m["trailer_km"]
-        trailer_cost = (TRAILER_BASE_COST + max(0, trailer_km - TRAILER_INCLUDED_KM) * TRAILER_KM_RATE) * 1.15
-        equipment_cost += qty * ((hours * rate) + trailer_cost)
+    for eq in equipment_inputs:
+        hours = eq["hours"]
+        trailer_km = eq["trailer_km"]
+        hourly_rate = eq["hourly_rate"]
+        trailer_cost = (TRAILER_BASE + max(0, trailer_km - TRAILER_INCLUDED_KM) * TRAILER_KM_RATE) * 1.15
+        equipment_cost += (hours * hourly_rate) + trailer_cost
 
-    vehicle_travel_cost = travel_km["vehicles"] * travel_km["km"] * VEHICLE_KM_RATE
+    travel_cost = travel_inputs["vehicles"] * travel_inputs["km"] * VEHICLE_KM_RATE
 
-    subtotal = sod_cost + pallet_cost + labor_cost + equipment_cost + vehicle_travel_cost
-    total = subtotal * (1 + HST_RATE)
+    gravel_cost = 0
+    if gravel_inputs["loads"] > 0:
+        extra_km = max(0, gravel_inputs["km"] - TRAILER_INCLUDED_KM)
+        delivery_fee = (TRAILER_BASE + extra_km * TRAILER_KM_RATE) * 1.15
+        gravel_cost = gravel_inputs["loads"] * (gravel_inputs["price"] + delivery_fee)
+
+    stay_cost = stay_inputs["nights"] * stay_inputs["rate"]
+
+    subtotal = sod_cost + pallet_cost + labor_cost + equipment_cost + travel_cost + gravel_cost + stay_cost
+    total = subtotal * (1 + HST)
 
     return {
         "area": area,
@@ -76,7 +82,9 @@ def calculate_sod_quote(area, laborers, labor_rate, labor_hours, machines, trave
         "pallet_cost": pallet_cost,
         "labor_cost": labor_cost,
         "equipment_cost": equipment_cost,
-        "travel_cost": vehicle_travel_cost,
+        "travel_cost": travel_cost,
+        "gravel_cost": gravel_cost,
+        "stay_cost": stay_cost,
         "subtotal": subtotal,
         "total": total
     }
